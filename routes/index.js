32174a798a7781c33eaaa68c7e9885ee8bc585f7
heru09 <heruprasetyo96@gmail.com>
2019-03-07 02:23:46 +0700

    UBUNTU LTS 18.04

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var pug = require('pug');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
 module.exports = router;
