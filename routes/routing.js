871c03523fa29f60f6e50431475ea222f8ce9693
heru09 <heruprasetyo96@gmail.com>
2019-03-06 09:32:12 +0700

    UBUNTU LTS 18.04

--- routes/routing.js
+++ routes/routing.js
@@ -1,8 +1,8 @@
 var express = require ('express')
 var app = express()
 
-app.post('/', function(req, res){
-    res.send('POST request to the home page');
+app.get('/', function(req, res){
+    res.send('error');
 })
 
 module.exports = app
